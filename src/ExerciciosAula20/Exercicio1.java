fd8bc79bbbcdaec2767649cee95d79abb497ae77
Júnior Matos <juniormatoss1967@hotmail.com>
2023-04-28 22:03:29 -0300

    Update Exercicio1.java

--- src/ExerciciosAula20/Exercicio1.java
+++ src/ExerciciosAula20/Exercicio1.java
@@ -3,7 +3,7 @@ import java.util.random;
 
 public class Exercicio1 {
     public static void main(String [] args){
-        double [][] M = new double [4] [4]; 
+        int [][] M = new int [4] [4]; 
         for (int i = 0; i < m.length; i++){
             m[i][i]
         }
