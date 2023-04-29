86997a393b08dbafb261ec9027f6e593488ee4e4
Júnior Matos <juniormatoss1967@hotmail.com>
2023-04-28 21:56:18 -0300

    aula20

---
+++ src/ExerciciosAula20/Exercicio1.java
@@ -0,0 +1,12 @@
+package ExerciciosAula20;
+import java.util.random;
+
+public class Exercicio1 {
+    public static void main(String [] args){
+        double [][] M = new double [4] [4]; 
+        for (int i = 0; i < m.length; i++){
+            m[i][i]
+        }
+    }
+    
+}
---
+++ src/ExerciciosAula20/Matrizes.java
@@ -0,0 +1,15 @@
+package ExerciciosAula20;
+
+public class Matrizes{
+    public static void main(String [] args){
+        double[][] notasAlunos= new double [30] [4];
+
+
+        notasAlunos [0][0] = 10;
+        notasAlunos [0][1] = 7;
+        notasAlunos [0][2] = 9;
+        notasAlunos [0][3] = 5;
+
+        System.out.println(notasAlunos[0][2]);
+    }
+}
