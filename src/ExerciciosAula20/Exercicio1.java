4a2218c3ac77f7ff1e522c6b462a27da7d48ef8f
Júnior Matos <juniormatoss1967@hotmail.com>
2023-05-08 13:26:58 -0300

    ok

--- src/ExerciciosAula20/Exercicio1.java
+++ src/ExerciciosAula20/Exercicio1.java
@@ -1,12 +1,34 @@
-package ExerciciosAula20;
-import java.util.random;
+import java.util.Random;
 
 public class Exercicio1 {
-    public static void main(String [] args){
-        int [][] M = new int [4] [4]; 
-        for (int i = 0; i < m.length; i++){
-            m[i][i]
+    public static void main(String[] args) {
+        int[][] numerosAleatorios = new int[4][4];
+
+        Random numeroRandom = new Random();
+
+        for (int i = 0; i < numerosAleatorios.length; i++) {
+            for (int j = 0; i < m[i].length; j++) {
+                numerosAleatorios[i][j] = numeroRandom.nextInt(100);
+
+            }
         }
+        int maiorNumero = 0;
+        int linha = 0;
+        int coluna = 0;
+        for (int i = 0; i < numerosAleatorios.length; i++) {
+            for (int j = 0; i < m[i].length; j++) {
+                if (m[i][j] > maiorNumero) {
+                    maiorNumero = m[i][j];
+                    linha = i;
+                    coluna = j;
+                }
+
+            }
+        }
+
+        System.out.println("Maior valor = " + maiorNumero);
+        System.out.println("Linha = " + linha);
+        System.out.println("Coluna = " + coluna);
     }
-    
+
 }
--- src/HelloWord.java
+++ src/HelloWord.java
@@ -2,4 +2,5 @@ public class HelloWord {
     public static void main(String [] args){
         System.out.print("Hello Word");
     }
+    
 }
