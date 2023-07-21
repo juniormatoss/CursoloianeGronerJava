4b3df3ce5bcb4008397b1159c50c79191d79ad1d
Júnior Matos <juniormatoss1967@hotmail.com>
2023-07-20 22:21:15 -0300

    Create Carro.java

---
+++ src/ExerciciosAula27/Carro/Carro.java
@@ -0,0 +1,30 @@
+package ExerciciosAula27.Carro;
+
+public class Carro {
+    
+
+    String marca;
+    String modelo;
+    int numPassageiros;
+    double capCombustivel;
+    double consumoCombustivel;
+
+
+    void exibitAutonomia(){
+        System.out.println("á autonomia é de: " + capCombustivel * consumoCombustivel);
+    }
+
+    double obterautonomia(){
+        System.out.println("Método obter altonomia foi chamado");
+        
+        return (capCombustivel * consumoCombustivel);
+    }
+    
+    double calcularCombustivel( double km){
+        
+        double qtdCombustivel= km / consumoCombustivel;
+        return qtdCombustivel;
+         
+    }
+
+}
