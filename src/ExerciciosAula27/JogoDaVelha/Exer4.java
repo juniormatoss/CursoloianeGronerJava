929d21fd899ac5beae5dd2bc01166ce50e6e7941
Júnior Matos <juniormatoss1967@hotmail.com>
2023-07-27 08:30:55 -0300

    Update Exer4.java

--- src/ExerciciosAula27/JogoDaVelha/Exer4.java
+++ src/ExerciciosAula27/JogoDaVelha/Exer4.java
@@ -5,26 +5,17 @@ import java.util.Scanner;
 public class Exer4 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        JogoDaVelha jogodavelha = new JogoDaVelha();
 
-
-    }
-
-}
-
+        System.out.println("Jogador 1 = X");
+        System.out.println("Jogador 2 = O");
 
         boolean ganhou = false;
         char sinal;
         int linha = 0;
         int coluna = 0;
 
-        
-
-        JogoDaVelha jogodavelha = new JogoDaVelha();
-
-        System.out.println("Jogador 1 = X");
-        System.out.println("Jogador 2 = O");
-
-        while (ganhou = true) {
+        while (!ganhou) {
             if (jogodavelha.vezJogador1()) {
                 System.out.println("Vez do jogador 1 - escolha linha e coluna para jogar(1-3)");
                 sinal = 'x';
@@ -34,10 +25,10 @@ public class Exer4 {
                 sinal = 'o';
             }
         }
-    }
-        
 
-        int valor(String tipoValor, Scanner scan) {
+    }
+
+    int valor(String tipoValor, Scanner scan) {
         int valor = 0;
         boolean valorValida = false;
         while (!valorValida) {
@@ -50,4 +41,6 @@ public class Exer4 {
             }
         }
         return valor;
-    
+    }
+
+}
