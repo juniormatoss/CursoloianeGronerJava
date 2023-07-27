9691fa47d9a2f165339637bc232e6eb69cdaadd4
Júnior Matos <juniormatoss1967@hotmail.com>
2023-07-27 08:40:11 -0300

    Update JogoDaVelha.java

--- src/ExerciciosAula27/JogoDaVelha/JogoDaVelha.java
+++ src/ExerciciosAula27/JogoDaVelha/JogoDaVelha.java
@@ -6,7 +6,8 @@ public class JogoDaVelha {
     int jogada = 1;
 
     boolean validarJogada(int linha, int coluna, char sinal) {
-        if (jogoDaVelha[linha][coluna] == 'X' || jogoDaVelha[linha][coluna] == 'O' || jogoDaVelha[linha][coluna] == 'x'|| jogoDaVelha[linha][coluna] == 'o') {
+        if (jogoDaVelha[linha][coluna] == 'X' || jogoDaVelha[linha][coluna] == 'O' || jogoDaVelha[linha][coluna] == 'x'
+                || jogoDaVelha[linha][coluna] == 'o') {
             return false;
         } else {
             jogoDaVelha[linha][coluna] = sinal;
@@ -15,21 +16,21 @@ public class JogoDaVelha {
         }
     }
 
-    void imprimitTabuleiro(){
+    void imprimitTabuleiro() {
         System.out.println("");
-            System.out.println("Tabuleiro Atualmente");
-            System.out.println("");
-            for (int i = 0; i < jogoDaVelha.length; i++) {
-                for (int j = 0; j < jogoDaVelha[i].length; j++) {
-                    System.out.print(jogoDaVelha[i][j] + "|");
-                }
-                System.out.println();
+        System.out.println("Tabuleiro Atualmente");
+        System.out.println("");
+        for (int i = 0; i < jogoDaVelha.length; i++) {
+            for (int j = 0; j < jogoDaVelha[i].length; j++) {
+                System.out.print(jogoDaVelha[i][j] + "|");
             }
+            System.out.println();
+        }
     }
 
-    boolean verificarGanhador(char sinal){
-        if  ((   jogoDaVelha[0][0] == sinal && jogoDaVelha[0][1] == sinal && jogoDaVelha[0][2] == sinal) || // linha1
-                (jogoDaVelha[1][0] == sinal && jogoDaVelha[1][1] == sinal && jogoDaVelha[1][2] == sinal) || // linha2   
+    boolean verificarGanhador(char sinal) {
+        if ((jogoDaVelha[0][0] == sinal && jogoDaVelha[0][1] == sinal && jogoDaVelha[0][2] == sinal) || // linha1
+                (jogoDaVelha[1][0] == sinal && jogoDaVelha[1][1] == sinal && jogoDaVelha[1][2] == sinal) || // linha2
                 (jogoDaVelha[2][0] == sinal && jogoDaVelha[2][1] == sinal && jogoDaVelha[2][2] == sinal) || // linha3
 
                 (jogoDaVelha[0][0] == sinal && jogoDaVelha[1][0] == sinal && jogoDaVelha[2][0] == sinal) || // coluna1
@@ -43,11 +44,10 @@ public class JogoDaVelha {
         return false;
     }
 
-
-    boolean vezJogador1(){
-        if (jogada % 2 == 1){
+    boolean vezJogador1() {
+        if (jogada % 2 == 1) {
             return true;
-        }else{
+        } else {
             return false;
         }
     }
