27d7225ef71ce95476525ec77407d154032f1974
Júnior Matos <juniormatoss1967@hotmail.com>
2023-07-26 22:35:13 -0300

    Create JogoDaVelhaTest.java

---
+++ src/ExerciciosAula27/JogoDaVelha/JogoDaVelhaTest.java
@@ -0,0 +1,49 @@
+package ExerciciosAula27.JogoDaVelha;
+
+import java.util.Scanner;
+
+public class JogoDaVelhaTest {
+    public static void main(String[] args) {
+
+        boolean ganhou = false;
+        char sinal;
+        int linha = 0;
+        int coluna = 0;
+
+        Scanner scan = new Scanner(System.in);
+
+        JogoDaVelha jogodavelha = new JogoDaVelha();
+
+        System.out.println("Jogador 1 = X");
+        System.out.println("Jogador 2 = O");
+
+        while (ganhou = true) {
+            if (jogodavelha.vezJogador1()) {
+                System.out.println("Vez do jogador 1 - escolha linha e coluna para jogar(1-3)");
+                sinal = 'x';
+
+            } else {
+                System.out.println("Vez do jogador 2 - escolha linha e coluna para jogar(1-3)");
+                sinal = 'o';
+            }
+        }
+    }
+        
+
+        int valor(String tipoValor, Scanner scan) {
+        int valor = 0;
+        boolean valorValida = false;
+        while (!valorValida) {
+            System.out.println("Entre com a " + tipoValor + " (1, 2 ou 3)");
+            valor = scan.nextInt();
+            if (valor >= 1 && valor <= 3) {
+                valorValida = true;
+            } else {
+                System.out.println("entrava Inválida, tente novamente");
+            }
+        }
+        return valor;
+    
+    }
+
+}
