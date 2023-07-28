4b0fda43fbf7043193a00b1c11b5e0cd4c40771d
Júnior Matos <juniormatoss1967@hotmail.com>
2023-07-28 20:28:42 -0300

    Create MinhaCalculadora.java

---
+++ src/ExerciciosAula33/MinhaCalculadora.java
@@ -0,0 +1,14 @@
+package ExerciciosAula33;
+
+public class MinhaCalculadora {
+    
+
+    public double soma(double num1, double num2){
+        return num1+ num2;
+    }
+
+    public double soma(double num1, double num2, double num3){
+        double calc = num1+num2+num3;
+        return calc;
+    }
+}
