51e0a6df606ededbc467ecc961e0eeb455524ca0
Júnior Matos <juniormatoss1967@hotmail.com>
2023-07-28 19:35:11 -0300

    Create TesteCarro.java

---
+++ src/ExerciciosAula31/TesteCarro.java
@@ -0,0 +1,11 @@
+package ExerciciosAula31;
+
+public class TesteCarro {
+    public static void main(String [] args){
+        Carro van =  new Carro();
+
+        van.marca = "fiat";
+    }
+
+    
+}
