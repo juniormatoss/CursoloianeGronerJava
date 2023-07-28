79bc5764bf32b89ccdb269e7ea83d0c52dfeb579
Júnior Matos <juniormatoss1967@hotmail.com>
2023-07-28 20:28:40 -0300

    Create TesteCalculadora.java

---
+++ src/ExerciciosAula33/TesteCalculadora.java
@@ -0,0 +1,12 @@
+package ExerciciosAula33;
+
+public class TesteCalculadora {
+    public static void main(String [] args){
+        MinhaCalculadora calc = new MinhaCalculadora();
+
+        double soma = calc.soma(5, 7);
+
+        System.out.print(soma);
+    }
+    
+}
