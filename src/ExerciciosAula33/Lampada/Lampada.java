4f4bce3f6955869f0c790569e17ba2af6cabef03
Júnior Matos <juniormatoss1967@hotmail.com>
2023-07-28 21:22:47 -0300

    Create Lampada.java

---
+++ src/ExerciciosAula33/Lampada/Lampada.java
@@ -0,0 +1,17 @@
+package ExerciciosAula33.Lampada;
+
+public class Lampada {
+
+    String cor;
+    String marca;
+    String modelo;
+
+
+    public void ligar(){
+        System.out.println("Lâmpada Ligada");
+    }
+    public void desligada(){
+        System.out.println("Lâmpada Desligada");
+    }
+    
+}
