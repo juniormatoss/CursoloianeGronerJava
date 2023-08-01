3bf51841caa6bd2fc5d6815c2d96ebd12039f771
Júnior Matos <juniormatoss1967@hotmail.com>
2023-07-31 23:22:14 -0300

    Update Teste.java

--- src/ExerciciosAula33/JogoDaVelha.java/Teste.java
+++ src/ExerciciosAula33/JogoDaVelha.java/Teste.java
@@ -1,3 +1,20 @@
+import java.util.Scanner;
+
 public class Teste {
+   
+    
+    public static void main(String [] args){
+
+        Scanner scan = new Scanner(System.in);
+        JogoDaVelha jogodavelha = new JogoDaVelha();
+
+        System.out.println("Jogador 1 = X");
+        System.out.println("Jogador 2 = O");
+
+        boolean ganhou = false;
+        char sinal;
+        int linha = 0;
+        int coluna = 0;
+
     
 }
