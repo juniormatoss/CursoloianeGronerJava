e81ccdd2695f2125598a45ac62c4885b3c69a13f
Júnior Matos <juniormatoss1967@hotmail.com>
2023-08-04 08:43:44 -0300

    Update Contador.java

--- src/ExerciciosAula34/Contador.java
+++ src/ExerciciosAula34/Contador.java
@@ -4,15 +4,49 @@ public class Contador{
     
     String nome;
     String funcao;
-    int idade;
-    String formacao;
+    int idade;  
+    String formacao;   
     String conhecimentoTecnico;
 
+    
     public Contador(String nome, String funcao, int idade, String formacao, String conhecimentoTecnico) {
         this.nome = nome;
         this.funcao = funcao;
         this.idade = idade;
         this.formacao = formacao;
+        this.conhecimentoTecnico = conhecimentoTecnico;
+    }
+    public Contador(){
+        
+    }
+    public String getNome(){
+        return this.nome;
+    }
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+    public String getFuncao(){
+        return this.funcao;
+    }
+    public void setFuncao(){
+        this.funcao = funcao;
+    }
+    public int getIdade() {
+        return idade;
+    }
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+    public String getFormacao() {
+        return formacao;
+    }
+    public void setFormacao(String formacao) {
+        this.formacao = formacao;
+    }
+    public String getConhecimentoTecnico() {
+        return conhecimentoTecnico;
+    }
+    public void setConhecimentoTecnico(String conhecimentoTecnico) {
         this.conhecimentoTecnico = conhecimentoTecnico;
     }
 
