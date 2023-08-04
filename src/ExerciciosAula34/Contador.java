cffce119050f034a590c3e8c07292d808d3f63ff
Júnior Matos <juniormatoss1967@hotmail.com>
2023-08-04 08:37:59 -0300

    Update Contador.java

--- src/ExerciciosAula34/Contador.java
+++ src/ExerciciosAula34/Contador.java
@@ -1,9 +1,20 @@
 package ExerciciosAula34;
 
-public class Contador {
-
-    public static cont(){
-
-    }
+public class Contador{
     
+    String nome;
+    String funcao;
+    int idade;
+    String formacao;
+    String conhecimentoTecnico;
+
+    public Contador(String nome, String funcao, int idade, String formacao, String conhecimentoTecnico) {
+        this.nome = nome;
+        this.funcao = funcao;
+        this.idade = idade;
+        this.formacao = formacao;
+        this.conhecimentoTecnico = conhecimentoTecnico;
+    }
+
+
 }
