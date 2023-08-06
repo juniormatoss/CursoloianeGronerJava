803619a233b0c10c4a352f8b17e350956dfcbdd9
Júnior Matos <juniormatoss1967@hotmail.com>
2023-08-06 19:29:33 -0300

    Create Test.java

---
+++ src/ExerciciosAula34/Test.java
@@ -0,0 +1,22 @@
+package ExerciciosAula34;
+
+public class Test {
+    public static void main (String [] args){
+          Contador cont = new Contador();
+
+         cont.incrementar();
+
+         cont.Zerar();
+
+         cont.incrementar();
+         cont.incrementar();
+         cont.incrementar();
+         cont.incrementar();
+         cont.incrementar();
+
+         System.out.println(cont.obterValor());
+    }
+    
+  
+
+}
