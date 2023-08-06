cd8d28cbda4e6d438badf1cb181129df47683fa8
Júnior Matos <juniormatoss1967@hotmail.com>
2023-08-06 19:21:31 -0300

    Update Contador.java

--- src/ExerciciosAula34/Contador.java
+++ src/ExerciciosAula34/Contador.java
@@ -4,6 +4,17 @@ public class Contador{
     
    static int cont;
 
-   
+
+    public void Zerar(){
+        cont = 0;
+    }
+
+    public void incrementar(int numero){
+        cont = cont + numero;
+    }
+    public int getCont(){
+        return this.cont;
+    }
+
 
 }
