f49aa7ebe21e876e9af813033df85ebaf71a306a
Júnior Matos <juniormatoss1967@hotmail.com>
2023-08-07 11:01:00 -0300

    Create Test.java

---
+++ src/ExerciciosAula34/Calculadora/Test.java
@@ -0,0 +1,14 @@
+package ExerciciosAula34.Calculadora;
+
+public class Test {
+    public static void main(String [] args){
+        Calculadora calculadora = new Calculadora();
+
+    calculadora.somar(10, 15);
+
+    System.out.println(calculadora.somar);
+}
+
+    }
+    
+    
