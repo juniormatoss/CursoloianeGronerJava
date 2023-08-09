874623bf8d260970c8666a4943f74746a108aed3
Júnior Matos <juniormatoss1967@hotmail.com>
2023-08-09 09:07:00 -0300

    Update ConversaoDeUnidadeDeVolume.java

--- src/ExerciciosAula34/Calculadora/ConversaoDeUnidadeDeVolume.java
+++ src/ExerciciosAula34/Calculadora/ConversaoDeUnidadeDeVolume.java
@@ -3,12 +3,45 @@ package ExerciciosAula34.Calculadora;
 public class ConversaoDeUnidadeDeVolume {
     
 
-    public static double ConversaoDeUnidadesDeVolume(double numero){
+    public static double centimetrosCubicos(double numero){
             double guardar = 0;
             for (int i = 0; i < numero; i++){
-                guardar = guardar + 929;
+                guardar = guardar + 1000;
             }
             return guardar;
         
     }
+    public static double litros(double numero){
+            double guardar = 0;
+            for (int i = 0; i < numero; i++){
+                guardar = guardar + 1000;
+            }
+            return guardar;
+        
+    }
+    public static double metroCubico(double numero){
+            double guardar = 0;
+            for (int i = 0; i < numero; i++){
+                guardar = guardar + 35.32;
+            }
+            return guardar;
+        
+    }
+    public static double galaoAmericanoPolegadas(double numero){
+            double guardar = 0;
+            for (int i = 0; i < numero; i++){
+                guardar = guardar + 231;
+            }
+            return guardar;
+        
+    }
+    public static double galaoAmericanoLitros(double numero){
+            double guardar = 0;
+            for (int i = 0; i < numero; i++){
+                guardar = guardar + 3.785;
+            }
+            return guardar;
+        
+    }
+
 }
