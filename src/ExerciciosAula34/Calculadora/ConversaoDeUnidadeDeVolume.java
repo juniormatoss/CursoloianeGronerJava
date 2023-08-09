ec199eef230238c2381b0fe893967b2cef7ba194
Júnior Matos <juniormatoss1967@hotmail.com>
2023-08-09 09:04:33 -0300

    Create ConversaoDeUnidadeDeVolume.java

---
+++ src/ExerciciosAula34/Calculadora/ConversaoDeUnidadeDeVolume.java
@@ -0,0 +1,14 @@
+package ExerciciosAula34.Calculadora;
+
+public class ConversaoDeUnidadeDeVolume {
+    
+
+    public static double ConversaoDeUnidadesDeVolume(double numero){
+            double guardar = 0;
+            for (int i = 0; i < numero; i++){
+                guardar = guardar + 929;
+            }
+            return guardar;
+        
+    }
+}
