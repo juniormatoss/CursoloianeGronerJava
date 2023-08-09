0ad810d7312ba4d646dedef572bdd1c0ac45555e
Júnior Matos <juniormatoss1967@hotmail.com>
2023-08-09 09:53:15 -0300

    Update TesteFibo.java

--- src/ExerciciosAula35/TesteFibo.java
+++ src/ExerciciosAula35/TesteFibo.java
@@ -2,6 +2,11 @@ package ExerciciosAula35;
 
 public class TesteFibo {
     public static void main(String [] args){
-        System.out.println(Fibonacci.fibo(3));
+
+        for (int i = 0; i < 10; i++){
+            System.out.print(Fibonacci.fibo(i) + " ");
+        }
+
+        
     }
 }
