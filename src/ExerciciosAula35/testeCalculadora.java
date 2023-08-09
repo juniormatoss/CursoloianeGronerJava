33c8ab5edcd3b3901286adc6d5967bab1391492a
Júnior Matos <juniormatoss1967@hotmail.com>
2023-08-09 09:28:37 -0300

    Create testeCalculadora.java

---
+++ src/ExerciciosAula35/testeCalculadora.java
@@ -0,0 +1,9 @@
+package ExerciciosAula35;
+
+public class testeCalculadora {
+    public static void main(String [] args){
+        int fatorialNR = Calculadora.fatorial(5);
+        System.out.println(fatorialNR);
+    }
+    
+}
