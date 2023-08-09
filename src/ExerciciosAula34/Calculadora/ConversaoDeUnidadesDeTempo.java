ebd845c037b2e0c64dce80a3af4dfb5616cb4fd8
Júnior Matos <juniormatoss1967@hotmail.com>
2023-08-09 09:13:18 -0300

    Update ConversaoDeUnidadesDeTempo.java

--- src/ExerciciosAula34/Calculadora/ConversaoDeUnidadesDeTempo.java
+++ src/ExerciciosAula34/Calculadora/ConversaoDeUnidadesDeTempo.java
@@ -2,10 +2,45 @@ package ExerciciosAula34.Calculadora;
 
 public class ConversaoDeUnidadesDeTempo {
     
-    public static double minutos(double numero){ 
+    public static double segundo(double numero){ 
             double guardar = 0;
             for (int i = 0; i < numero; i++){
                 guardar = guardar + 60;
+            }
+            return guardar;
+    }
+    public static double hora(double numero){ 
+            double guardar = 0;
+            for (int i = 0; i < numero; i++){
+                guardar = guardar + 60;
+            }
+            return guardar;
+    }
+    public static double dia(double numero){ 
+            double guardar = 0;
+            for (int i = 0; i < numero; i++){
+                guardar = guardar + 24;
+            }
+            return guardar;
+    }
+    public static double semana(double numero){ 
+            double guardar = 0;
+            for (int i = 0; i < numero; i++){
+                guardar = guardar + 7;
+            }
+            return guardar;
+    }
+    public static double mes(double numero){ 
+            double guardar = 0;
+            for (int i = 0; i < numero; i++){
+                guardar = guardar + 30;
+            }
+            return guardar;
+    }
+    public static double ano(double numero){ 
+            double guardar = 0;
+            for (int i = 0; i < numero; i++){
+                guardar = guardar + 365.25;
             }
             return guardar;
     }
