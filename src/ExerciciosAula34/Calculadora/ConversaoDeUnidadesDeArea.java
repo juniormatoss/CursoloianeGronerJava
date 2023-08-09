ce1ecaa3922994a0ca048cff9b5d9ac6cf7b4eaa
Júnior Matos <juniormatoss1967@hotmail.com>
2023-08-09 09:01:06 -0300

    Create ConversaoDeUnidadesDeArea.java

---
+++ src/ExerciciosAula34/Calculadora/ConversaoDeUnidadesDeArea.java
@@ -0,0 +1,35 @@
+package ExerciciosAula34.Calculadora;
+
+public class ConversaoDeUnidadesDeArea {
+
+    public double metroQuadrado(int metro){
+        double guardar = 0;
+        for (int i = 0; i < metro; i++){
+            guardar = guardar + 10.76;
+        }
+        return guardar;
+
+    }
+    public double peQuadrado (double numero){
+        double guardar = 0;
+        for (int i = 0; i < numero; i++){
+            guardar = guardar + 929;
+        }
+        return guardar;
+    }
+    public double milhaQuadrada (double numero){
+        double guardar = 0;
+        for (int i = 0; i < numero; i++){
+            guardar = guardar + 640;
+        }
+        return guardar;
+    }
+    public double acre (double numero){
+        double guardar = 0;
+        for (int i = 0; i < numero; i++){
+            guardar = guardar + 43.560;
+        }
+        return guardar;
+    }
+    
+}
