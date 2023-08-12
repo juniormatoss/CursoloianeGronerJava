2aaee701c5c6b332d2b6464fccb4eb1ea74fb538
Júnior Matos <juniormatoss1967@hotmail.com>
2023-08-11 23:02:09 -0300

    ok

--- src/ExerciciosAula36/Contato.java
+++ src/ExerciciosAula36/Contato.java
@@ -3,9 +3,17 @@ package ExerciciosAula36;
 public class Contato {
 
    
-    String nome;
-    int telefone;
-    String email;
+    private String nome;
+    private int telefone;
+    private String email;
+
+    public String getNome(){
+        return this.nome;
+    }
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+    
 
     
 }
--- src/ExerciciosAula36/TesteAgenda.java
+++ src/ExerciciosAula36/TesteAgenda.java
@@ -5,6 +5,10 @@ public class TesteAgenda {
     public static void main (String [] args){
 
         Scanner scan = new Scanner(System.in);
+
+        System.out.println("Entre com o nome da agenda");
+        String nome = scan.nextLine();
+        Agenda agenda = new Agenda(nome);
     }
     
 }
