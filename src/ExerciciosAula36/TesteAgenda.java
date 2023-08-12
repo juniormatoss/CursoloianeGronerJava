810cb6b670695adbbfa0d0c30ea023e49ba843eb
Júnior Matos <juniormatoss1967@hotmail.com>
2023-08-11 23:24:55 -0300

    Update TesteAgenda.java

--- src/ExerciciosAula36/TesteAgenda.java
+++ src/ExerciciosAula36/TesteAgenda.java
@@ -9,6 +9,30 @@ public class TesteAgenda {
         System.out.println("Entre com o nome da agenda");
         String nome = scan.nextLine();
         Agenda agenda = new Agenda(nome);
+
+        Contato[] contatos = new Contato[3];
+
+        for (int i = 0; i < contatos.length; i++){
+            System.out.println("Digite o contato " + (i+1));
+            Contato c = new Contato();
+            
+            System.out.println("Entre com o nome");
+            nome = scan.nextLine();
+            c.setNome(nome);
+            
+            System.out.println("Entre com o Telefone");
+            String telefone = scan.nextLine();
+            c.setTelefone(i);
+
+            System.out.println("Entre com o Email");
+            String email = scan.nextLine();
+            c.setEmail(nome);
+
+            contatos[i] = c;
+        }
+        agenda.setContatos(contatos);
+
+        
     }
     
 }
