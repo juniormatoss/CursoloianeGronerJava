f047c36210c5e03e3f81bd1826573830c90d3af6
Júnior Matos <juniormatoss1967@hotmail.com>
2023-08-11 23:02:51 -0300

    Update Contato.java

--- src/ExerciciosAula36/Contato.java
+++ src/ExerciciosAula36/Contato.java
@@ -13,7 +13,20 @@ public class Contato {
     public void setNome(String nome){
         this.nome = nome;
     }
+    public int getTelefone() {
+        return telefone;
+    }
+    public void setTelefone(int telefone) {
+        this.telefone = telefone;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
     
+
 
     
 }
