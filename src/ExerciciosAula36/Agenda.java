797322eb1a0825409641b689cc44a4e1506451e0
Júnior Matos <juniormatoss1967@hotmail.com>
2023-08-11 23:24:51 -0300

    Update Agenda.java

--- src/ExerciciosAula36/Agenda.java
+++ src/ExerciciosAula36/Agenda.java
@@ -1,29 +1,43 @@
 package ExerciciosAula36;
 
 public class Agenda {
-    
+
     private String nome;
-    public Contato [] contatos;
+    public Contato[] contatos;
 
-
-    public String nome(){
+    public String nome() {
         return this.nome;
     }
-    public void nome(String nome){
+
+    public void nome(String nome) {
         this.nome = nome;
     }
-    
+
     public Contato[] getContatos() {
         return contatos;
     }
+
     public void setContatos(Contato[] contatos) {
         this.contatos = contatos;
     }
-    public Agenda(){
+
+    public Agenda() {
 
     }
-    public Agenda(String nome){
+
+    public Agenda(String nome) {
         this.nome = nome;
+    }
+
+    public String obterinformacoes() {
+        String info = "nome = " + nome + "\n";
+        if (contatos != null) {
+            for (Contato c : contatos) {
+                info += c.obertInformacoes() + "\n";
+            }
+        }
+
+        return info;
     }
 
 }
