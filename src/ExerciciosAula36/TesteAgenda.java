b1518b459d4de54ce19a26a6a13b664699b5af4b
Júnior Matos <juniormatoss1967@hotmail.com>
2023-08-13 21:18:47 -0300

    Update TesteAgenda.java

--- src/ExerciciosAula36/TesteAgenda.java
+++ src/ExerciciosAula36/TesteAgenda.java
@@ -32,6 +32,8 @@ public class TesteAgenda {
         }
         agenda.setContatos(contatos);
         if(agenda != null){
+
+            
             
             System.out.println(agenda.obterinformacoes());
 
