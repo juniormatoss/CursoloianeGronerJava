a60b68bb82dacd58cc2c077d47e335718d22eca0
Júnior Matos <juniormatoss1967@hotmail.com>
2023-08-20 07:24:03 -0300

    Update Exercicio1.java

--- src/ExerciciosAula13/Exercicio1.java
+++ src/ExerciciosAula13/Exercicio1.java
@@ -2,6 +2,7 @@ package ExerciciosAula13;
 
 public class Exercicio1 {
     public static void main(String [] args){
-        System.out.prin("hello wold");
+        System.out.print("hello wold");
+        System.out.println("");
     }
 }
